656c4dacec551892d7f93e523d7eb2d71e8a707b
Cong Cuong PHAM <ccuong.ph@gmail.com>
2021-10-08 16:03:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 //Install express server
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
 const app = express();
-app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/covid-mashup'));
